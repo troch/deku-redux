826c968072309e5198ee0f2b3a30f264eb1d6cb0
Thomas Roch <thomas.c.roch@gmail.com>
2015-11-12 18:53:05 +0000

    test: add storePlugin tests

--- test/main.js
+++ test/main.js
@@ -1,16 +1,55 @@
 require('mocha');
+var should = require('should');
+
+var Mock = require('component-mock');
+
+var redux = require('redux');
+var createStore = redux.createStore;
+
+var element = require('virtual-element');
+var deku = require('deku');
+var tree = deku.tree;
 
 var dekuRedux = require('../dist/commonjs');
 var storePlugin = dekuRedux.storePlugin;
 var connect = dekuRedux.connect;
 
+var store = createStore(function(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1 };
+        default:
+            return state;
+    }
+}, { count: 0 });
+
+function increment() {
+    return {
+        type: 'INCREMENT'
+    };
+}
+
+function render(component) {
+    return element('div', component.props.count);
+}
+
 describe('deku-redux', function() {
     it('should add store and storeState to a deku tree', function() {
-        // import { tree, render } from 'deku';
-        // const app = tree()
-        //     .use(storePlugin(store))
-        //     .mount(element(App));
+        var app = tree()
+            .use(storePlugin(store))
+            .mount(element({ render: render}));
 
+        app.sources.store.should.equal(store);
+        app.sources.storeState.should.eql({ count: 0 });
+
+        store.dispatch(increment());
+
+        app.sources.storeState.should.eql({ count: 1 });
+        // console.log(app);
+
+        // var mock = Mock(app);
+        // var r = mock.render();
+        // console.log(r);
         // render(app, document.getElementById('app'));
     });
 });
